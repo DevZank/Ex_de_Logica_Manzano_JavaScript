bab590eef42b37a15eaba05eb6845a1c039b297f
Isaac Martins <111926496+DevZank@users.noreply.github.com>
2024-06-05 14:31:43 -0300

    Update script.js

--- PAGINA 66/EX-A/script.js
+++ PAGINA 66/EX-A/script.js
@@ -6,5 +6,5 @@ let resultado = 0
 
 for (let contadora = 15; contadora < 201; contadora++) {
     resultado = contadora * contadora
-    console.log(`Contadora ^ 2 = ${resultado}`)
+    console.log(`${contadora} ^ 2 = ${resultado}`)
 }
